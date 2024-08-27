a6a921679d40d98f084d4fcc5450a0cda9ed6b01
Joshua <hola@joshua.cl>
2024-08-26 20:24:43 -0400

    Sección 11: Sistema de pedidos - BACKEND

--- Sección 11 Sistema de pedidos - BACKEND/E-Commerce + Next + Strapi + Stripe/ecommerce-strapi/types/generated/contentTypes.d.ts
+++ Sección 11 Sistema de pedidos - BACKEND/E-Commerce + Next + Strapi + Stripe/ecommerce-strapi/types/generated/contentTypes.d.ts
@@ -866,6 +866,44 @@ export interface ApiGameGame extends Schema.CollectionType {
   };
 }
 
+export interface ApiOrderOrder extends Schema.CollectionType {
+  collectionName: 'orders';
+  info: {
+    singularName: 'order';
+    pluralName: 'orders';
+    displayName: 'Order';
+    description: '';
+  };
+  options: {
+    draftAndPublish: false;
+  };
+  attributes: {
+    user: Attribute.Relation<
+      'api::order.order',
+      'oneToOne',
+      'plugin::users-permissions.user'
+    >;
+    totalPayment: Attribute.Decimal & Attribute.Required;
+    idPayment: Attribute.String;
+    addressShipping: Attribute.JSON & Attribute.Required;
+    products: Attribute.JSON & Attribute.Required;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::order.order',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::order.order',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface ApiPlatformPlatform extends Schema.CollectionType {
   collectionName: 'platforms';
   info: {
@@ -959,6 +997,7 @@ declare module '@strapi/types' {
       'plugin::i18n.locale': PluginI18NLocale;
       'api::address.address': ApiAddressAddress;
       'api::game.game': ApiGameGame;
+      'api::order.order': ApiOrderOrder;
       'api::platform.platform': ApiPlatformPlatform;
       'api::wishlist.wishlist': ApiWishlistWishlist;
     }
